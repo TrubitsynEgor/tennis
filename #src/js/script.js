4053a47d638f79cbbae802fd0a8ef341fb948c77
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-01-18 22:25:44 +0500

    mobile menu full + js + adaptive

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,2 +1,29 @@
 'use strict'
 
+const menuBtnOpen = document.querySelector('.mobile-munu__btn--open');
+const menuBtnClose = document.querySelector('.mobile-munu__btn--close');
+const menuList = document.querySelector('.mobile-munu__list');
+const body = document.querySelector('body');
+
+menuBtnOpen.addEventListener('click', () => {
+	menuList.classList.add('active');
+	menuBtnOpen.classList.add('active');
+	menuBtnClose.classList.add('active');
+	body.classList.add('lock')
+})
+menuBtnClose.addEventListener('click', () => {
+	menuList.classList.remove('active');
+	menuBtnClose.classList.remove('active');
+	menuBtnOpen.classList.remove('active');
+	body.classList.remove('lock')
+})
+menuList.addEventListener('click', function (e) {
+	let target = e.target;
+	if (!target) {
+		menuList.classList.remove('active');
+		menuBtnClose.classList.remove('active');
+		menuBtnOpen.classList.remove('active');
+		body.classList.remove('lock')
+	}
+})
+
